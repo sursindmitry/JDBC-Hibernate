1adb38832099aacb4ee39b354fa94d2b1fffb3a2
Dmitry Sursin <40328886+sursindmitry@users.noreply.github.com>
2024-05-31 23:55:40 +0700

    task-1: JDBC implementation (#1)

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,52 @@
 package jm.task.core.jdbc.service;
 
+import java.util.List;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
-import java.util.List;
-
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
 
+    UserDao userDao = new UserDaoJDBCImpl();
+
+    public void createUsersTable() {
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Имя не может быть пустым или null");
+        }
 
+        if (lastName == null || lastName.isEmpty()) {
+            throw new IllegalArgumentException("Фамилия не может быть пустой или null");
+        }
+
+        if (age < 0 || age > 120) {
+            throw new IllegalArgumentException("Возраст должен быть между 0 и 120");
+        }
+
+        userDao.saveUser(name, lastName, age);
+        System.out.println("User с именем - " + name + " добавлен в базу данных");
     }
 
     public void removeUserById(long id) {
+        if (id < 1){
+            throw new RuntimeException("ID не может быть меньше 1");
+        }
 
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-
+        userDao.cleanUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,43 @@
 package jm.task.core.jdbc.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соединения с БД
+    public static Connection connect() {
+        Properties properties = new Properties();
+
+        try (InputStream inputStream = Util.class.getClassLoader()
+            .getResourceAsStream("application.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            try {
+                connection = DriverManager.getConnection(
+                    properties.getProperty("host"),
+                    properties.getProperty("user"),
+                    properties.getProperty("password")
+                );
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (connection != null) {
+            return connection;
+        } else {
+            throw new RuntimeException("Вы не смогли подключится к БД проверьте настройки");
+        }
+    }
 }

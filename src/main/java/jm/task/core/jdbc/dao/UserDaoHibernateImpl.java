5ad928c409e205fd64415cdef65885dd6e017211
Dmitry Sursin <dmitrijsursin@gmail.com>
2024-06-01 13:04:05 +0700

    task-2: Hibernate implementation

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,130 @@
 package jm.task.core.jdbc.dao;
 
-import jm.task.core.jdbc.model.User;
-
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.NativeQuery;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
+    private final SessionFactory factory;
 
+    public UserDaoHibernateImpl() {
+        this.factory = Util.setUp();
     }
 
 
     @Override
     public void createUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            NativeQuery<?> query = session.createSQLQuery(
+                "CREATE TABLE IF NOT EXISTS users(" +
+                    "id BIGSERIAL PRIMARY KEY," +
+                    "name VARCHAR(512)," +
+                    "lastname VARCHAR(512)," +
+                    "age SMALLINT)");
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Не удалось создать таблицу users", e);
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            NativeQuery<?> query = session.createNativeQuery(
+                "DROP TABLE IF EXISTS users"
+            );
+            query.executeUpdate();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Не удалось удалить таблицу users", e);
+        }
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        User user = new User(name, lastName, age);
 
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+
+            session.save(user);
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Не удалось добавить пользователя с именем - " + name, e);
+        }
     }
 
     @Override
     public void removeUserById(long id) {
 
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+
+            User user = Optional.ofNullable(session.get(User.class, id))
+                .orElseThrow(() -> new RuntimeException("Нет пользователя с ID - " + id));
+
+            session.delete(user);
+
+            transaction.commit();
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка при удалении пользователя", e);
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+
+            List<User> allUsers = new ArrayList<>(session.createQuery("FROM User").getResultList());
+
+            transaction.commit();
+
+            return allUsers;
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка при получении пользователей", e);
+        }
     }
 
     @Override
     public void cleanUsersTable() {
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
 
+            NativeQuery<?> query = session.createNativeQuery(
+                "TRUNCATE TABLE users RESTART IDENTITY CASCADE"
+            );
+            query.executeUpdate();
+
+            transaction.commit();
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка при очистке таблицы пользователей", e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,21 +6,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Util {
-    public static Connection connect() {
-        Properties properties = new Properties();
 
-        try (InputStream inputStream = Util.class.getClassLoader()
-            .getResourceAsStream("application.properties")) {
-            properties.load(inputStream);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public static Connection connect() {
+        Properties properties = getProperties();
+        loadDriverClass();
 
         Connection connection = null;
-        try {
-            Class.forName("org.postgresql.Driver");
             try {
                 connection = DriverManager.getConnection(
                     properties.getProperty("host"),
@@ -30,14 +29,54 @@ public class Util {
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
 
         if (connection != null) {
             return connection;
         } else {
             throw new RuntimeException("Вы не смогли подключится к БД проверьте настройки");
+        }
+    }
+
+    public static SessionFactory setUp() {
+        Properties properties = getProperties();
+        loadDriverClass();
+
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .applySetting("hibernate.connection.url", properties.getProperty("hibernate.connection.url"))
+            .applySetting("hibernate.connection.username", properties.getProperty("hibernate.connection.username"))
+            .applySetting("hibernate.connection.password", properties.getProperty("hibernate.connection.password"))
+            .applySetting("hibernate.show_sql", properties.getProperty("hibernate.show_sql"))
+            .build();
+        try {
+            MetadataSources sources = new MetadataSources(registry);
+            sources.addAnnotatedClass(User.class);
+
+            Metadata metadata = sources.getMetadataBuilder().build();
+
+            return metadata.getSessionFactoryBuilder().build();
+        } catch (Exception e) {
+            StandardServiceRegistryBuilder.destroy(registry);
+            throw new RuntimeException("Вы не смогли подключится к БД проверьте настройки");
+        }
+    }
+
+    private static Properties getProperties() {
+        Properties properties = new Properties();
+
+        try (InputStream inputStream = Util.class.getClassLoader()
+            .getResourceAsStream("application.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return properties;
+    }
+
+    private static void loadDriverClass() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
